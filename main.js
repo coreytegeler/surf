aeee234fe8753a6bd97999a6e18553e37bafe975
coreytegeler <coreytegeler@gmail.com>
2015-01-31 14:15:22 -0500

    loading search query

--- main.js
+++ main.js
@@ -1,25 +1,38 @@
-
-
 var player;
 function onYouTubeIframeAPIReady() {
 	player = new YT.Player('player', {
 	  height: '390',
 	  width: '640',
-	  videoId: 'k51cnVcbUAo',
 	  playerVars: {
 	  	'autoplay' : 1,
 	  	'controls' : 0
 	  },
 	  events: {
-	    'onReady': onPlayerReady
+	    'onReady': getId
 	  }
+	});
+}
+
+//https://developers.google.com/youtube/v3/docs/search/list
+function getId() {
+	var key = 'AIzaSyD7UE-orpOJW1DBo6Z-rAMCAEjQbVZEvfg';
+	var order = 'rating';
+	var query = 'lol';
+	var count = 20;
+	var yt = 'https://www.googleapis.com/youtube/v3/search?order=' + order + '&part=id&q=' + query + '&maxResults=' + count + '&key=' + key;
+
+	$.get( yt, function(data) {
+	  var videos = data.items;
+	  var index = Math.floor((Math.random() * count) + 1);
+	  var video = videos[index];
+	  var id = video.id.videoId;
+	  player.videoId = id;
 	});
 }
 
 function onPlayerReady(event) {
 	var vid = event.target;
 	vid.playVideo();
-	vid.setVolume(0);
 }
 
 function stopVideo() {
